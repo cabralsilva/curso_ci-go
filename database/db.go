1662d63bda9e27c8ba2110a87194f78819d235ec
DanielDeuPetch <daniel@deupetch.com.br>
2024-02-22 16:11:24 -0300

    testes 103

--- database/db.go
+++ database/db.go
@@ -14,7 +14,7 @@ var (
 )
 
 func ConectaComBancoDeDados() {
-	stringDeConexao := "host=" + os.Getenv("DATABASE_URL") + " user=" + os.Getenv("DATABASE_USER") + " password=" + os.Getenv("DATABASE_PWD") + " dbname=" + os.Getenv("DATABASE_DBNAME") + " port=" + os.Getenv("DATABASE_DBPORT") + " sslmode=disable"
+	stringDeConexao := "host=" + os.Getenv("DATABASE_URL") + " user=" + os.Getenv("DATABASE_USER") + " password=" + os.Getenv("DATABASE_PWD") + " dbname=" + os.Getenv("DATABASE_DBNAME") + " port=" + os.Getenv("DATABASE_DBPORT")
 	DB, err = gorm.Open(postgres.Open(stringDeConexao))
 	if err != nil {
 		log.Panic("Erro ao conectar com banco de dados: " +  "host=" + os.Getenv("DATABASE_URL") + " user=" + os.Getenv("DATABASE_USER") + " password=" + os.Getenv("DATABASE_PWD") + " dbname=" + os.Getenv("DATABASE_DBNAME") + " port=" + os.Getenv("DATABASE_DBPORT") + " sslmode=disable")
@@ -23,3 +23,6 @@ func ConectaComBancoDeDados() {
 
 	DB.AutoMigrate(&models.Aluno{})
 }
+
+
+// host=root.c5kq0ywq2g7o.us-east-1.rds.amazonaws.com user=postgres password=123456789 dbname=root port=5432 sslmode=disable
